7539ae0b0ca9072b825e5fe2a3fd9da7ad715d67
Zihao Yang <yangzihao@ncti-gba.cn>
2023-07-10 15:51:47 +0800

    修复实例监控和智能诊断前端BUG

--- plugins/observability-instance/web-ui/src/pages/dashboardV2/resourceMonitor/Index.vue
+++ plugins/observability-instance/web-ui/src/pages/dashboardV2/resourceMonitor/Index.vue
@@ -1,51 +1,50 @@
 <template>
-    <div style="margin-top: 6px;">
-        <IndexBar :tabId="props.tabId"></IndexBar>
-        <el-tabs v-model="resourceMonitorTabIndex" type="card" class="card-tabs">
-            <el-tab-pane label="CPU" :name="tabKeys.ResourceMonitorCPU">
-                <CPU
-                    :tabId="props.tabId"
-                    v-if="
-                        tabKeyLoaded.indexOf(tabKeys.ResourceMonitorCPU) >= 0 ||
-                        resourceMonitorTabIndex === tabKeys.ResourceMonitorCPU
-                    "
-                    @goto="goto"
-                >
-                </CPU>
-            </el-tab-pane>
-            <el-tab-pane :label="$t('resourceMonitor.memoryTab')" :name="tabKeys.ResourceMonitorMemory">
-                <Memory
-                    :tabId="props.tabId"
-                    v-if="
-                        tabKeyLoaded.indexOf(tabKeys.ResourceMonitorMemory) >= 0 ||
-                        resourceMonitorTabIndex === tabKeys.ResourceMonitorMemory
-                    "
-                >
-                </Memory>
-            </el-tab-pane>
-            <el-tab-pane :label="$t('resourceMonitor.ioTab')" :name="tabKeys.ResourceMonitorIO">
-                <IO
-                    :tabId="props.tabId"
-                    v-if="
-                        tabKeyLoaded.indexOf(tabKeys.ResourceMonitorIO) >= 0 ||
-                        resourceMonitorTabIndex === tabKeys.ResourceMonitorIO
-                    "
-                >
-                </IO>
-            </el-tab-pane>
-            <el-tab-pane :label="$t('resourceMonitor.networkTab')" :name="tabKeys.ResourceMonitorNetwork">
-                <Network
-                    :tabId="props.tabId"
-                    v-if="
-                        tabKeyLoaded.indexOf(tabKeys.ResourceMonitorNetwork) >= 0 ||
-                        resourceMonitorTabIndex === tabKeys.ResourceMonitorNetwork
-                    "
-                ></Network
-            ></el-tab-pane>
-        </el-tabs>
-    </div>
-
-    <div style="margin-bottom: 20px"></div>
+  <div style="margin-top: 6px">
+    <IndexBar :tabId="props.tabId"></IndexBar>
+    <el-tabs v-model="resourceMonitorTabIndex" type="card" class="card-tabs">
+      <el-tab-pane label="CPU" :name="tabKeys.ResourceMonitorCPU">
+        <CPU
+          :tabId="props.tabId"
+          v-if="
+            tabKeyLoaded.indexOf(tabKeys.ResourceMonitorCPU) >= 0 ||
+            resourceMonitorTabIndex === tabKeys.ResourceMonitorCPU
+          "
+          @goto="goto"
+        >
+        </CPU>
+      </el-tab-pane>
+      <el-tab-pane :label="$t('resourceMonitor.memoryTab')" :name="tabKeys.ResourceMonitorMemory">
+        <Memory
+          :tabId="props.tabId"
+          v-if="
+            tabKeyLoaded.indexOf(tabKeys.ResourceMonitorMemory) >= 0 ||
+            resourceMonitorTabIndex === tabKeys.ResourceMonitorMemory
+          "
+        >
+        </Memory>
+      </el-tab-pane>
+      <el-tab-pane :label="$t('resourceMonitor.ioTab')" :name="tabKeys.ResourceMonitorIO">
+        <IO
+          :tabId="props.tabId"
+          v-if="
+            tabKeyLoaded.indexOf(tabKeys.ResourceMonitorIO) >= 0 ||
+            resourceMonitorTabIndex === tabKeys.ResourceMonitorIO
+          "
+        >
+        </IO>
+      </el-tab-pane>
+      <el-tab-pane :label="$t('resourceMonitor.networkTab')" :name="tabKeys.ResourceMonitorNetwork">
+        <Network
+          :tabId="props.tabId"
+          v-if="
+            tabKeyLoaded.indexOf(tabKeys.ResourceMonitorNetwork) >= 0 ||
+            resourceMonitorTabIndex === tabKeys.ResourceMonitorNetwork
+          "
+        >
+        </Network>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -64,37 +63,37 @@ const tabKeyLoaded = ref<Array<string>>([])
 const resourceMonitorTabIndex = ref<string>('')
 // tab render only once, prevent the echarts turns into a little one
 watch(resourceMonitorTabIndex, (v) => {
-    if (tabKeyLoaded.value.indexOf(v) < 0) tabKeyLoaded.value.push(v)
+  if (tabKeyLoaded.value.indexOf(v) < 0) tabKeyLoaded.value.push(v)
 })
 
 const setNowTab = () => {
-    useMonitorStore(props.tabId).updateTabNow(resourceMonitorTabIndex.value)
+  useMonitorStore(props.tabId).updateTabNow(resourceMonitorTabIndex.value)
 }
 const outsideGoto = (key: string) => {
-    resourceMonitorTabIndex.value = key
+  resourceMonitorTabIndex.value = key
 }
 defineExpose({ outsideGoto })
 onMounted(() => {
-    resourceMonitorTabIndex.value = tabKeys.ResourceMonitorCPU
+  resourceMonitorTabIndex.value = tabKeys.ResourceMonitorCPU
 })
 watch(resourceMonitorTabIndex, (v) => {
-    setNowTab()
+  setNowTab()
 })
 const emit = defineEmits(['goto'])
 const goto = (key: string, param: object) => {
-    console.log('DEBUG: resourceMonitor param', param)
-    emit('goto', key, param)
+  console.log('DEBUG: resourceMonitor param', param)
+  emit('goto', key, param)
 }
 
 // same for every page in index
 watch(
-    updateCounter,
-    () => {
-        if (tabNow.value === tabKeys.ResourceMonitor) {
-            setNowTab()
-        }
-    },
-    { immediate: true }
+  updateCounter,
+  () => {
+    if (tabNow.value === tabKeys.ResourceMonitor) {
+      setNowTab()
+    }
+  },
+  { immediate: true }
 )
 // same for every page in index
 </script>
