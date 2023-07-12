2f3d178e8b9e288700bec925876052eca4bb9e7b
zhouwenruo <zhouwenruo@ncti-gba.cn>
2023-07-12 15:03:41 +0800

    删除关闭窗口清掉断点接口

--- plugins/data-studio/src/main/java/com/nctigba/datastudio/service/impl/debug/StepInImpl.java
+++ plugins/data-studio/src/main/java/com/nctigba/datastudio/service/impl/debug/StepInImpl.java
@@ -147,7 +147,6 @@ public class StepInImpl implements OperationInterface {
                 paramReq.setOldWindowName(name);
             }
             if (!oidList.contains(oid)) {
-                stepOut.deleteBreakPoint(webSocketServer, paramReq);
                 webSocketServer.sendMessage(windowName, closeWindow, SUCCESS, null);
                 DebugUtils.enableButton(webSocketServer, oldWindowName);
                 paramReq.setCloseWindow(true);
