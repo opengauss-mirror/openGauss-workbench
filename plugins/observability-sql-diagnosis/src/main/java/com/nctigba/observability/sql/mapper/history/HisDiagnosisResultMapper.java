49bd74333293963caad598eb4c8d96b5ab885bc5
罗梦 <luomeng@ncti-gba.cn>
2023-07-20 17:52:53 +0800

    codeCheck修改

--- plugins/observability-sql-diagnosis/src/main/java/com/nctigba/observability/sql/mapper/history/HisDiagnosisResultMapper.java
+++ plugins/observability-sql-diagnosis/src/main/java/com/nctigba/observability/sql/mapper/history/HisDiagnosisResultMapper.java
@@ -22,14 +22,23 @@ import java.util.List;
 @Mapper
 @DS(HisDiagnosisInit.HISTORY_DIAGNOSIS_RESULT)
 public interface HisDiagnosisResultMapper extends BaseMapper<HisDiagnosisResult> {
-    @Insert("<script>" +
-            "INSERT INTO his_diagnosis_result_info (id, cluster_id, node_id, task_id, point_name, point_type, point_title, point_suggestion, point_data, is_hint, point_detail, point_state, is_deleted, create_time, update_time) "
-            +
-            "VALUES " +
-            "<foreach collection='list' item='item' separator=','>" +
-            "(#{item.id}, #{item.clusterId}, #{item.nodeId}, #{item.taskId}, #{item.pointName}, #{item.pointType}, #{item.pointTitle}, #{item.pointSuggestion}, #{item.pointData, typeHandler = com.nctigba.common.mybatis.JacksonJsonWithClassTypeHandler}, #{item.isHint}, #{item.pointDetail}, #{item.pointState}, #{item.isDeleted}, #{item.createTime}, #{item.updateTime})"
-            +
-            "</foreach>" +
-            "</script>")
+    /**
+     * Batch insert data
+     *
+     * @param hisDiagnosisResult List of HisDiagnosisResult
+     */
+    @Insert("<script>"
+            + "INSERT INTO his_diagnosis_result_info (id, cluster_id, node_id, task_id, point_name, point_type, "
+            + "point_title, point_suggestion, point_data, is_hint, point_detail, point_state, is_deleted, create_time, "
+            + "update_time) "
+            + "VALUES "
+            + "<foreach collection='list' item='item' separator=','>"
+            + "(#{item.id}, #{item.clusterId}, #{item.nodeId}, #{item.taskId}, #{item.pointName}, #{item.pointType}, "
+            + "#{item.pointTitle}, #{item.pointSuggestion}, "
+            + "#{item.pointData, typeHandler = com.nctigba.common.mybatis.JacksonJsonWithClassTypeHandler}, "
+            + "#{item.isHint}, #{item.pointDetail}, #{item.pointState}, #{item.isDeleted}, #{item.createTime}, "
+            + "#{item.updateTime})"
+            + "</foreach>"
+            + "</script>")
     void batchInert(List<HisDiagnosisResult> hisDiagnosisResult);
 }
