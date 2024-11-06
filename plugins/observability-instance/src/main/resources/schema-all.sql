6756cad3ee8afafbf64aed6dc5882d6b284098e5
jinyuyang <jinyuyang@chinamobile.com>
2024-11-06 10:43:54 +0800

    对启动sql进行存在性检查

--- plugins/observability-instance/src/main/resources/schema-all.sql
+++ plugins/observability-instance/src/main/resources/schema-all.sql
@@ -1,13 +1,50 @@
-CREATE TABLE public.nctigba_env (
+CREATE TABLE IF NOT EXISTS public.nctigba_env (
 	id varchar NULL,
-	hostid varchar NULL,
-	"type" varchar NULL,
-	username varchar NULL,
-	"path" varchar NULL,
-	port int8 NULL
+    hostid varchar NULL,
+    "type" varchar NULL,
+    username varchar NULL,
+    "path" varchar NULL,
+    port int8 NULL
 );
-ALTER TABLE public.nctigba_env ADD nodeid varchar NULL;
-ALTER TABLE public.nctigba_env ADD status varchar NULL;
-ALTER TABLE public.nctigba_env ADD update_time timestamp NULL;
-ALTER TABLE public.nctigba_env ADD param varchar NULL;
-ALTER TABLE public.nctigba_env ADD err_status_msg varchar NULL;
+CREATE OR REPLACE FUNCTION alter_nctigba_env() RETURNS integer AS '
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM information_schema.columns
+        WHERE table_name=''nctigba_env'' AND column_name=''nodeid''
+    ) THEN
+        ALTER TABLE public.nctigba_env ADD COLUMN nodeid VARCHAR NULL;
+    END IF;
+
+    IF NOT EXISTS (
+        SELECT 1 FROM information_schema.columns
+        WHERE table_name=''nctigba_env'' AND column_name=''status''
+    ) THEN
+        ALTER TABLE public.nctigba_env ADD COLUMN status VARCHAR NULL;
+    END IF;
+
+    IF NOT EXISTS (
+        SELECT 1 FROM information_schema.columns
+        WHERE table_name=''nctigba_env'' AND column_name=''update_time''
+    ) THEN
+        ALTER TABLE public.nctigba_env ADD COLUMN update_time TIMESTAMP NULL;
+    END IF;
+
+    IF NOT EXISTS (
+        SELECT 1 FROM information_schema.columns
+        WHERE table_name=''nctigba_env'' AND column_name=''param''
+    ) THEN
+        ALTER TABLE public.nctigba_env ADD COLUMN param VARCHAR NULL;
+    END IF;
+
+    IF NOT EXISTS (
+        SELECT 1 FROM information_schema.columns
+        WHERE table_name=''nctigba_env'' AND column_name=''err_status_msg''
+    ) THEN
+        ALTER TABLE public.nctigba_env ADD COLUMN err_status_msg VARCHAR NULL;
+    END IF;
+    RETURN 0;
+END'
+LANGUAGE plpgsql;
+
+SELECT alter_nctigba_env();
+DROP FUNCTION alter_nctigba_env();
