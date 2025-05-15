b7bd6c0f1a3639d24ebe6bd9d88b25aa5e84afed
zhangzhihui42 <zhangzhihui42@huawei.com>
2025-05-15 11:16:09 +0800

    修复sudo用户启动录制回放问题

--- plugins/data-migration/src/main/java/org/opengauss/admin/plugin/handler/TranscribeReplayHandle.java
+++ plugins/data-migration/src/main/java/org/opengauss/admin/plugin/handler/TranscribeReplayHandle.java
@@ -91,7 +91,7 @@ public class TranscribeReplayHandle {
         String installPath = getInstallPath(transcribeReplayTask, taskType);
         String configFilePath = installPath + "/config/" + taskType + ".properties";
         commandSb.append("cd ").append(installPath);
-        commandSb.append(" && (java -Xms256m -Xmx2g -jar ").append(jarPath);
+        commandSb.append(" && (sudo java -Xms256m -Xmx2g -jar ").append(jarPath);
         commandSb.append(" -t ").append(taskType);
         commandSb.append(" -f ").append(configFilePath);
         commandSb.append(" 2>&1 | tee ").append(String.format("%s_result.log", taskType));
