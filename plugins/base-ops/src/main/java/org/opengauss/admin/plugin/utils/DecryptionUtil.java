12a6fe99e21a0444957f56443e8ead55b4562d17
zhangzhihui42 <zhangzhihui42@huawei.com>
2025-07-15 20:21:54 +0800

    fix install and import

--- plugins/base-ops/src/main/java/org/opengauss/admin/plugin/utils/DecryptionUtil.java
+++ plugins/base-ops/src/main/java/org/opengauss/admin/plugin/utils/DecryptionUtil.java
@@ -31,12 +31,15 @@ import org.opengauss.admin.common.exception.ops.OpsException;
 import org.opengauss.admin.plugin.domain.model.ops.EnterpriseInstallConfig;
 import org.opengauss.admin.plugin.domain.model.ops.ImportClusterBody;
 import org.opengauss.admin.plugin.domain.model.ops.InstallBody;
+import org.opengauss.admin.plugin.domain.model.ops.InstallContext;
 import org.opengauss.admin.plugin.domain.model.ops.LiteInstallConfig;
 import org.opengauss.admin.plugin.domain.model.ops.MinimalistInstallConfig;
 import org.opengauss.admin.plugin.enums.ops.OpenGaussVersionEnum;
 import org.opengauss.admin.system.service.ops.impl.EncryptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
 
 /**
  * DecryptionUtil
@@ -57,7 +60,7 @@ public class DecryptionUtil {
      */
     public void decryptDatabasePassword(InstallBody installBody) {
         OpenGaussVersionEnum openGaussVersion = installBody.getInstallContext().getOpenGaussVersion();
-        decryptDatabasePassword(installBody.getInstallContext(), openGaussVersion);
+        decryptDatabasePassword(installBody.getInstallContext(), openGaussVersion, true);
     }
 
     /**
@@ -67,42 +70,40 @@ public class DecryptionUtil {
      */
     public void decryptDatabasePassword(ImportClusterBody importClusterBody) {
         OpenGaussVersionEnum openGaussVersion = importClusterBody.getOpenGaussVersion();
-        decryptDatabasePassword(importClusterBody, openGaussVersion);
+        decryptDatabasePassword(importClusterBody, openGaussVersion, false);
     }
 
-    private void decryptDatabasePassword(Object installConfig, OpenGaussVersionEnum openGaussVersion) {
+    private void decryptDatabasePassword(Object installConfig, OpenGaussVersionEnum openGaussVersion,
+        boolean isInstallBody) {
         switch (openGaussVersion) {
             case ENTERPRISE:
-                if (installConfig instanceof EnterpriseInstallConfig) {
-                    EnterpriseInstallConfig enterpriseConfig = (EnterpriseInstallConfig) installConfig;
-                    enterpriseConfig.setDatabasePassword(
-                        encryptionUtils.decrypt(enterpriseConfig.getDatabasePassword()));
-                } else {
-                    throw new IllegalArgumentException(
-                        "Expected EnterpriseInstallConfig but got " + installConfig.getClass().getName());
-                }
+                EnterpriseInstallConfig enterpriseConfig = getInstallConfig(installConfig, isInstallBody,
+                    InstallContext::getEnterpriseInstallConfig, ImportClusterBody::getEnterpriseInstallConfig);
+                enterpriseConfig.setDatabasePassword(encryptionUtils.decrypt(enterpriseConfig.getDatabasePassword()));
                 break;
             case MINIMAL_LIST:
-                if (installConfig instanceof MinimalistInstallConfig) {
-                    MinimalistInstallConfig minimalistConfig = (MinimalistInstallConfig) installConfig;
-                    minimalistConfig.setDatabasePassword(
-                        encryptionUtils.decrypt(minimalistConfig.getDatabasePassword()));
-                } else {
-                    throw new IllegalArgumentException(
-                        "Expected MinimalistInstallConfig but got " + installConfig.getClass().getName());
-                }
+                MinimalistInstallConfig minimalistConfig = getInstallConfig(installConfig, isInstallBody,
+                    InstallContext::getMinimalistInstallConfig, ImportClusterBody::getMinimalistInstallConfig);
+                minimalistConfig.setDatabasePassword(encryptionUtils.decrypt(minimalistConfig.getDatabasePassword()));
                 break;
             case LITE:
-                if (installConfig instanceof LiteInstallConfig) {
-                    LiteInstallConfig liteConfig = (LiteInstallConfig) installConfig;
-                    liteConfig.setDatabasePassword(encryptionUtils.decrypt(liteConfig.getDatabasePassword()));
-                } else {
-                    throw new IllegalArgumentException(
-                        "Expected LiteInstallConfig but got " + installConfig.getClass().getName());
-                }
+                LiteInstallConfig liteConfig = getInstallConfig(installConfig, isInstallBody,
+                    InstallContext::getLiteInstallConfig, ImportClusterBody::getLiteInstallConfig);
+                liteConfig.setDatabasePassword(encryptionUtils.decrypt(liteConfig.getDatabasePassword()));
                 break;
             default:
                 throw new OpsException("unsupported version：[" + openGaussVersion + "]");
+        }
+    }
+
+    private <T> T getInstallConfig(Object installConfig, boolean isInstallBody,
+        Function<InstallContext, T> installBodyMapper, Function<ImportClusterBody, T> importClusterBodyMapper) {
+        if (isInstallBody && installConfig instanceof InstallContext installCfg) {
+            return installBodyMapper.apply(installCfg);
+        } else if (!isInstallBody && installConfig instanceof ImportClusterBody importClusterBody) {
+            return importClusterBodyMapper.apply(importClusterBody);
+        } else {
+            throw new IllegalArgumentException("Invalid installConfig type or mismatched flag");
         }
     }
 }
